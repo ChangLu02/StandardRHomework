d504dbbff9fea6effb6c945da6d5556067652b0c
ChangLu02 <lclouis@bu.edu>
2024-09-06 16:05:42 -0400

    Homework

---
+++ HW1.R
@@ -0,0 +1,52 @@
+### YOUR NAME HERE ###
+
+### Homework 1                                         ###
+### Standard R ###
+
+#Put your code in this file. Make sure you assign the relevant values to the correct variable names, which are given below. 
+#Uncomment the variables as you assign your final values/functions/results to them.
+
+### VECTORS ###
+## Q1 
+
+#v1 <- 
+#v2 <- 
+#v3 <- 
+  
+### MATRICES ###
+
+## Q2 
+  
+#matA<-
+
+## Q3
+
+#matB<-
+#a3<-
+#b3<-
+
+## Q4
+
+#tmpFn<-
+
+## Q5
+
+#quadmap<-
+
+## Q6
+
+#In1<-
+#In2<-
+
+## Q7
+
+#fibonacci<-
+
+## Q8
+
+#n<-
+
+## Q9
+
+#rowAdd<-
+  
